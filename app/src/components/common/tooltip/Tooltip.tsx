a1bc41bfb3a8e6cf5c671875709b263b73882f6a
Ivaylo Zdravkov <133565939+ivajlozdr@users.noreply.github.com>
2025-01-23 18:28:02 +0200

    fix name

--- app/src/components/common/tooltip/Tooltip.tsx
+++ app/src/components/common/tooltip/Tooltip.tsx
@@ -4,7 +4,7 @@ interface Props {
   message: string;
 }
 
-const Technologies: React.FC<Props> = ({ message }) => {
+const Tooltip: React.FC<Props> = ({ message }) => {
   return (
     <div
       className="absolute bg-white dark:bg-[#313335] bottom-full left-1/2 transform -translate-x-1/2 mb-3 px-3 py-1 text-defaulttextcolor text-sm rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none shadow-lg"
@@ -19,4 +19,4 @@ const Technologies: React.FC<Props> = ({ message }) => {
   );
 };
 
-export default Technologies;
+export default Tooltip;
