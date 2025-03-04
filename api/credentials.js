b5d827fb4c68a3759f2fda6fb53d0f3088fb507e
Ivaylo Zdravkov <133565939+ivajlozdr@users.noreply.github.com>
2025-03-04 15:50:53 +0200

    ...
    This reverts commit e948e4199f5810f2796b4674571ef1351c26d7e4.

--- api/credentials.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  SECRET_KEY: "1a2b3c4d5e6f7g8h9i0jklmnopqrstuvwxyz123456",
-  EMAIL_USER: "no-reply@kariero.noit.eu",
-  EMAIL_PASS: "Noit_2025",
-  DEEPL_API_KEY: "d0a3cc80-3e72-49d9-a0d6-4a7de4df7b6f:fx",
-  CUSTOM_SEARCH_API_KEY: "AIzaSyBkQKjvwEUYdDYHX7u0PNYa_9MWEIOHzfk",
-  CX: "160b0be643d1045a6",
-  ONET_API_KEY: "cGdpOjk1Njlwdmg="
-};
